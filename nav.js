5b15f48898bbdfa3ba020002d2a17ce4a1dd970b
engo <63791277+joeengo@users.noreply.github.com>
2021-12-20 16:13:00 +0000

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,5 +1,6 @@
 fetch('/joeengo.github.io/nav.html')
 .then(res => res.text())
+
 .then(text => {
     let oldelem = document.querySelector("script#replace_with_navbar");
     let newelem = document.createElement("div");
